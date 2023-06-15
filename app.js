595acbdacf869e6c03dbea0cd3948494cebba57f
Jovan-Nsty <jova.zyo@gmail.com>
2023-06-15 16:05:52 +0200

    Implemented Book on page display

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const main = document.querySelector('#main');
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -14,3 +16,20 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.unshift(book);
 }
+
+function displayLibrary() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const content = document.createElement('div');
+    content.classList.add('card');
+    content.textContent = myLibrary[i].info();
+    main.appendChild(content);
+  }
+}
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 650, 'read');
+const book2 = new Book('Cujo', 'Stephen King', 425, 'not read yet');
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+displayLibrary();
